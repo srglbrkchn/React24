506b97865347de575c7e0d606ca8387c2018df2b
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-27 21:49:17 +0100

    Add - button, increase the count variable use useState Hook.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,18 @@ function App() {
     let [count, setCount] = useState(0);
 
     function increase() {
-      
       setCount(count + 1);
+    }
+
+    function decrease() {
+      setCount (count - 1);
     }
 
     return (
         <div className="container">
             <h1>{count}</h1>
             <button onClick={increase}>+</button>
+            <button onClick={decrease}>-</button>
         </div>
     );
 }
