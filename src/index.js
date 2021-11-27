ec3483d92e36846731ca6862391ad6a5ee5f6c5f
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-27 19:08:02 +0100

    Set h1 value to count variable. Add onClick function to add button.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+let count = 0;
+
 ReactDOM.render(
   <div className="container">
-    <h1>0</h1>
-    <button>+</button>
+    <h1>{count}</h1>
+    <button onClick={()=> console.log("I got clicked.")}>+</button>
   </div>,
   document.getElementById("root")
 );
